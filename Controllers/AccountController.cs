91d7f0a94dbbc64aaefe4f5a5718976e4a41a244
john ardiente <officialjla@gmail.com>
2023-05-25 17:47:30 +0800

    Initial Commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using fms.Data;
+using fms.Models;
+using fms.ViewModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace fms.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AccountController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, bool rememberMe)
+        {
+            var properties = new AuthenticationProperties
+            {
+                AllowRefresh = false,
+                IsPersistent = rememberMe
+            };
+            if (ModelState.IsValid)
+            {
+                var user = await _dbContext.Users.SingleOrDefaultAsync(x=>x.Username == model.Username && x.Password == model.Password);
+                if(user != null)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.Username),
+                    };
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+                    await HttpContext.SignInAsync(principal, properties);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
+        }
+        // GET: /Account/Logout
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account"); // Redirect to the desired page after successful logout
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using fms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fms.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
