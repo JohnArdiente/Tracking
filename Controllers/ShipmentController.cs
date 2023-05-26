b17dc00eb7326c4bb8e9d5ff5315fc40505260cc
john ardiente <officialjla@gmail.com>
2023-05-26 14:56:57 +0800

    adding nav links

---
+++ Controllers/ShipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fms.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
